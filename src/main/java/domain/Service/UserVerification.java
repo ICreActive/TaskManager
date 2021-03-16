538385fd5aef76a19a0dddeac8ed9f581d1b21ce
ICreActive <negumadness@gmail.com>
2021-03-16 22:19:19 +0300

    Added: Serialization with user verification

---
+++ src/main/java/domain/Service/UserVerification.java
@@ -0,0 +1,21 @@
+package domain.Service;
+
+import domain.Users.User;
+
+import java.util.List;
+
+public class UserVerification {
+
+    public static void UserAddWithVerify(List<User> users, User user) {
+        boolean userArePresent = users.stream()
+                .anyMatch(userInDB -> userInDB.getEmail().equals(user.getEmail()));
+        if (!userArePresent) {
+            users.add(user);
+        }
+        else System.out.println("User with email " +user.getEmail() + " are present in DB");
+
+    }
+
+}
+
+
